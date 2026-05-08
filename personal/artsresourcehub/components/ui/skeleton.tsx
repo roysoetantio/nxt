817d42f0bab225ebb90ecbdc8b670c95301457bf
roysoetantio <roysoetantio@gmail.com>
2026-05-08 18:54:30 +0800

    Delete personal/artsresourcehub directory

--- personal/artsresourcehub/components/ui/skeleton.tsx
+++
@@ -1,8 +0,0 @@
-import { cn } from '@/lib/utils';
-import { View, type ViewProps } from 'react-native';
-
-function Skeleton({ className, ...props }: ViewProps) {
-  return <View className={cn('animate-pulse rounded-md bg-muted', className)} {...props} />;
-}
-
-export { Skeleton };
